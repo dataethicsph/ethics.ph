7f86dfb8e21728fd4b0676103ce5bffc015ebfff
Dominic Ligot <dominic.ligot@cirrolytix.com>
2026-02-13 13:30:27 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Data Ethics PH
+
+This repository is for the Data Ethics PH website. 
---
+++ site/index.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Data Ethics PH</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="index, follow">
+    <meta name="description" content="Data rights are human rights. Social problems are data problems">
+    <meta name="keywords" content="dominic vincent ligot, dominic ligot, doc ligot, data ethics philippines, social impact philippines, cirrolytix, entrepreneur philippines, artificial intelligence philippines, remote sensing philippines, social listening philippines, data for social good, analytics for social good, data value chain, analytics philippines, social impact philippines, project aedes, troglodyte, coronatracker, pcij, analytics association of the philippines, cirrolytix, ai philippines, ai, artificial intelligence, ai consultant philippines, ai influencer philippines, ai ethics, data ethics, hackathon, social impact hackathon">
+    <meta name="author" content="Dominic Vincent Ligot">
+    <meta property="article:published_time" content="2019-11-01" />
+    <meta property="og:url" content="https://ethics.ph/">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Data Ethics PH">
+    <meta property="og:description" content="Data rights are human rights. Social problems are data problems">
+    <meta property="og:image" content="https://ethics.ph/splash.jpg">
+    <!--<meta property="fb:app_id" content="317301752192662">-->
+    <meta name="twitter:image" content="https://ethics.ph/splash.jpg">
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+    <link rel="canonical" href="https://ethics.ph/">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+        .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+        .fa-anchor,.fa-coffee {font-size:200px}
+		#header1 {
+			background-image: url('images/header1.jpg');
+			min-height: 100%;
+			background-position: center;
+			background-size: cover;
+		}
+		#header2 {
+			background-image: url('images/header2.jpg');
+			min-height: 100%;
+			background-position: center;
+			background-size: cover;
+		}
+		#header3 {
+			background-image: url('images/header3.jpg');
+			min-height: 100%;
+			background-position: center;
+			background-size: cover;
+		}
+		#header4 {
+			background-image: url('images/header4.jpg');
+			min-height: 100%;
+			background-position: center;
+			background-size: cover;
+		}		
+		#footer {
+			background-image: url('images/header1.jpg');
+			min-height: 100%;
+			background-position: center;
+			background-size: cover;
+		}
+    </style>
+</head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E6WSMLW1XS"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-E6WSMLW1XS');
+</script>
+
+<body>
+
+    <!-- Navbar -->
+    <div class="w3-top">
+        <div class="w3-bar w3-deep-orange w3-card w3-left-align w3-large">
+            <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-deep-orange" href="javascript:void(0);" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+            <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white">Home</a>
+            <a href="#about" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">About</a>
+            <a href="#services" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Services</a>
+            <a href="#work" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Our Work</a>
+            <a href="#feedback" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Feedback</a>
+            <a href="#contact" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white">Contact</a>
+        </div>
+
+        <!-- Navbar on small screens -->
+        <div id="navDemo" class="w3-bar-block w3-white w3-hide w3-hide-large w3-hide-medium w3-large">
+            <a href="#about" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">About</a>
+            <a href="#services" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">Services</a>
+            <a href="#work" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">Our Work</a>
+            <a href="#feedback" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">Feedback</a>
+            <a href="#contact" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">Contact</a>
+        </div>
+    </div>
+
+    <!-- Header -->
+    <header>
+        <div id="header1" class="headerimage w3-container w3-deep-orange w3-center" style="padding:128px 16px">
+            <h1 class="w3-margin w3-jumbo">Data Ethics PH</h1>
+            <p class="w3-xlarge">We create data-driven content for an ethical society.</p>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdEQftjhWs1eDem_9WLjVe-ncJVuMuuO9j3aW_1MUMerVPW0w/viewform" target="_blank"><button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Contact Us</button></a>
+        </div>
+        <div id="header2" class="headerimage w3-container w3-deep-orange w3-center" style="padding:128px 16px; display: none;">
+            <h1 class="w3-margin w3-jumbo">Data Ethics PH</h1>
+            <p class="w3-xlarge">We create data-driven content for an ethical society.</p>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdEQftjhWs1eDem_9WLjVe-ncJVuMuuO9j3aW_1MUMerVPW0w/viewform" target="_blank"><button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Contact Us</button></a>
+        </div>
+        <div id="header3" class="headerimage w3-container w3-deep-orange w3-center" style="padding:128px 16px; display: none;">
+            <h1 class="w3-margin w3-jumbo">Data Ethics PH</h1>
+            <p class="w3-xlarge">We create data-driven content for an ethical society.</p>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdEQftjhWs1eDem_9WLjVe-ncJVuMuuO9j3aW_1MUMerVPW0w/viewform" target="_blank"><button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Contact Us</button></a>
+        </div>
+        <div id="header4" class="headerimage w3-container w3-deep-orange w3-center" style="padding:128px 16px; display: none;">
+            <h1 class="w3-margin w3-jumbo">Data Ethics PH</h1>
+            <p class="w3-xlarge">We create data-driven content for an ethical society.</p>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdEQftjhWs1eDem_9WLjVe-ncJVuMuuO9j3aW_1MUMerVPW0w/viewform" target="_blank"><button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Contact Us</button></a>
+        </div>
+    </header>
+
+    <!-- About Us -->
+    <div class="w3-row-padding w3-padding-64 w3-container" id="about">
+        <div class="w3-content">
+            <div class="w3-third w3-center w3-padding-large">
+                <img src="favicon.png" style="width: 75%;">
+            </div>
+            <div class="w3-twothird">
+                <h1>About Us</h1>
+                <h5 class="w3-padding-32">At Data Ethics PH, we believe in the power of media to promote awareness and support public discourse on the ethical use of data and AI. Our mission is to generate awareness about data privacy, data security, disinformation, and AI ethics to help build a better, more equitable digital world. With a focus on responsible journalism and ethical storytelling, we are committed to promoting positive change and creating a culture of data ethics.</h5>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Services -->
+    <div class="w3-row-padding w3-light-grey w3-padding-64 w3-container" id="services">
+        <div class="w3-content">
+            <div class="w3-twothird">
+                <h1>What We Do</h1>
+                <h5 class="w3-padding-32">We offer a range of services to promote ethical use of data and AI, including:
+                    <ul>
+                        <li>Content Creation: Our team of writers, journalists, and creative professionals produce high-quality content to generate awareness and promote public discourse on the ethical use of data and AI.</li>
+                        <li>Capacity Building: We offer training services to help individuals and organizations stay up-to-date with the latest trends and techniques in data ethics and AI.</li>
+                        <li>Community Engagement: We conduct community activities, including hackathons and data challenges, to encourage participants in the use of data and AI for social impact.</li>
+                    </ul>
+                </h5>
+            </div>
+            <div class="w3-third w3-center">
+                <img src="images/seminar.jpeg" style="width: 100%;">
+            </div>
+        </div>
+    </div>
+
+    <!-- Work -->
+    <div class="w3-row-padding w3-orange w3-padding-64 w3-container" id="work">
+        <div class="w3-content">
+            <div class="w3-center">
+                <h1>Our Work</h1>
+                <h5 class="w3-padding-32">Our content explores topics related to the ethical use of data and AI. We feature stories of data and AI used for good, as well as the challenges and milestones of advocates and innovators striving for a better society.</h5>
+            </div>
+            <div class="w3-row">
+                <div class="w3-half w3-padding-small">
+                    <iframe style="width: 100%; height: 315px;" src="https://www.youtube.com/embed/jFj4kNCZv-o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="w3-half w3-padding-small">
+                    <iframe style="width: 100%; height: 315px;" src="https://www.youtube.com/embed/YQmJHx-SSyA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="w3-half w3-padding-small">
+                    <iframe style="width: 100%; height: 315px;" src="https://www.youtube.com/embed/pN5q8RS-S_U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="w3-half w3-padding-small">
+                    <iframe style="width: 100%; height: 315px;" src="https://www.youtube.com/embed/oN5labxCe5Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="w3-half w3-padding-small">
+                    <iframe style="width: 100%; height: 315px;" src="https://www.youtube.com/embed/HsaguMWOOrI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                <div class="w3-half w3-padding-small">
+                    <iframe style="width: 100%; height: 315px;" src="https://www.youtube.com/embed/sJyLcyTzUso" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Feedback -->
+    <div class="w3-row-padding w3-padding-64 w3-container" id="feedback">
+        <div class="w3-content">
+            <div class="w3-third w3-center w3-padding">
+                <img src="images/approval.webp" style="width: 100%;">
+            </div>
+            <div class="w3-twothird">
+                <h1>Feedback</h1>
+                <h5 class="w3-padding-32"><i>Data Ethics PH has played a vital role in increasing knowledge and encouraging conversation regarding the ethical handling of data and AI in our organization. Their skilled team collaborated closely with us to create a customized content strategy that surpassed our expectations. We take pride in working with a company that shares our dedication to ethical journalism and responsible storytelling.</i> - International NGO</h5>
+                <h5 class="w3-padding-32"><i>Ang nililikha ng Data Ethics PH na content ay laging thought-provoking at insightful. They are truly committed sa pag-promote ng ethical use ng data at AI, at ang kanilang content ay nakakatulong sa pagbuo ng isang mas magandang at patas na digital na mundo.</i> - Digital Rights Advocate</h5>
+                <h5 class="w3-padding-32"><i>I wanted to take a moment to express my appreciation for the exceptional work that you do. As a government official, I have had the pleasure of working with your team on several occasions, and I must say, your commitment to promoting ethical use of data and AI is truly exceptional.<br/><br/>Your expertise and dedication to responsible journalism and ethical storytelling have been instrumental in raising awareness and promoting public discourse on data privacy, security, disinformation, and AI ethics. The customized solutions you offer have been invaluable in helping our government agency manage risk and make informed decisions.<br/><br/>It has been a pleasure working with a company that shares our values and dedication to responsible data management and building a better, more equitable digital world. I highly recommend Data Ethics PH to any government agency or organization looking to promote responsible data management and make a positive impact.<br/><br/>Thank you for all that you do.</i> - National Government Official</h5>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Contact -->
+    <div class="w3-row-padding w3-light-grey w3-padding-64 w3-container" id="contact">
+        <div class="w3-content">
+            <div class="w3-twothird">
+                <h1>Contact Us</h1>
+                <h5 class="w3-padding-32">If you are interested in learning more about our services or would like to discuss a specific project, please get in touch with us today. We would be happy to answer any questions you may have. 
+				</h5>
+				
+				<div class="w3-center"><a href="https://docs.google.com/forms/d/e/1FAIpQLSdEQftjhWs1eDem_9WLjVe-ncJVuMuuO9j3aW_1MUMerVPW0w/viewform" target="_blank"><button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Message Us</button></a></div>
+				
+                <div class="w3-xxlarge w3-section w3-text-black w3-center">
+                    <a href="mailto:info@ethics.ph"><i class="fa fa-envelope w3-hover-opacity"></i></a>
+                    <a href="https://twitter.com/ethicsph" target="_blank"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+                    <a href="https://www.linkedin.com/company/data-ethics-ph/" target="_blank"><i class="fa fa-linkedin w3-hover-opacity"></i></a>
+                    <a href="https://www.facebook.com/ethics.ph/" target="_blank"><i class="fa fa-facebook w3-hover-opacity"></i></a>
+                </div>
+            </div>
+            <div class="w3-third w3-center w3-padding">
+                <img src="images/contact.webp" style="width: 100%;">
+            </div>
+        </div>
+    </div>
+
+    <div id="footer" class="w3-container w3-black w3-center w3-opacity w3-padding-64">
+        <h1 class="w3-margin w3-xlarge">Data rights are human rights. Social problems are data problems.</h1>
+    </div>
+
+    <!-- Footer -->
+    <footer class="w3-container w3-padding-64 w3-center w3-opacity">
+        <p>&copy; 2019-2023 Data Ethics PH</p>
+    </footer>
+
+    <script>
+        // Used to toggle the menu on small screens when clicking on the menu button
+        function myFunction() {
+          var x = document.getElementById("navDemo");
+          if (x.className.indexOf("w3-show") == -1) {
+            x.className += " w3-show";
+          } else { 
+            x.className = x.className.replace(" w3-show", "");
+          }
+        }
+
+	function showSlides() {
+		var i;
+		var slides = document.getElementsByClassName("headerimage");
+		for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none";
+		}
+		slideIndex++;
+		if (slideIndex > slides.length) {
+			slideIndex = 1;
+		}
+		slides[slideIndex - 1].style.display = "block";
+		setTimeout(showSlides, 4500); 
+	}
+	var slideIndex = 0;
+	showSlides();
+    </script>
+</body>
+
+</html>
